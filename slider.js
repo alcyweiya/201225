783ac580ee217df72f6244322ff2e477071400bb
七夜 <alcyweiya@gmail.com>
2021-01-27 18:37:25 +0800

    210127：RWD & 版面調整完畢，跑馬燈大致over

--- slider.js
+++ slider.js
@@ -1,4 +1,32 @@
-left.onclick = function() 
-{
-document.getElementById('.slider div').animation-delay; this.dataset.delay;    
+
+
+onload  = start;
+
+function start()
+{	
+    var i = 1;
+    function Move()
+            { 	
+                i = (i%4)+1; // 4 is the Number of image in slider
+                document.getElementById('i'+i).checked = true;
+            }
+    setInterval(Move,3000); //change img in 3 sec
 }
+
+
+
+    var counter = 1;
+    setInterval(function(){
+          for(var i = 1; i < 4; i++)
+          {
+              if(document.getElementById('radio' + i).checked)
+              {
+                 counter = i+1;
+                 if(counter > 3){
+                 counter = 1;}
+  
+              }
+          }
+          document.getElementById('radio' + counter).checked = true;
+          
+        }, 8000);
